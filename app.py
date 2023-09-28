bf3ecdba7bc6629a79368d8d82e1d5ad5c14ff11
Eleanor Hayden <135467024+eshayden0617@users.noreply.github.com>
2023-09-27 21:34:52 -0500

    flask complete!

--- app.py
+++ app.py
@@ -1,38 +1,42 @@
 # 1. Import Flask
 from flask import Flask, render_template, jsonify
-import sqlite3
 import pandas as pd
 
 # 2. Create an app
 app = Flask(__name__)
 
-# 3. Connection to Database
-conn = sqlite3.connect('data/worldpopulation.')
-query = 'SELECT * FROM '
-df = pd.read_sql(query, conn)
-conn.close()
+# 3. Connection to CSV
+try:
+    df = pd.read_csv('data/worldpopulation.csv')
+except FileNotFoundError:
+    print("CSV file not found")
 
-# 4. Define static routes
+# 4. Define html routes
 @app.route("/")
 def index():
     return render_template("index.html")
 
+# 5. App route for animated map
+@app.route("/Density(2022)")
+def density_2022():
+    # Convert DataFrame to JSON
+    specific_columns = df[['Country', 'Density(2022)']]
+    animatedMapData = specific_columns.to_dict(orient='records')
+    return jsonify(animatedMapData)
 
-@app.route("/about")
-def about():
-    name = "Peleke"
-    location = "Tien Shan"
+# 6. App route for poppulation map
+@app.route("/Population")
+def populationMap():
+    specific_columns = df[['Country', '2022']]
+    populationMapData = specific_columns.to_dict(orient='records')
+    return jsonify(populationMapData)
 
-    return f"My name is {name}, and I live in {location}."
+# 4. App route for bar chart
+@app.route("/Country")
+def yearsbyCountry():
+    specific_columns = df[['Country', '1970', '1980', '1990', '2000', '2010', '2015', '2020', '2022']]
+    countryBarChart = specific_columns.to_dict(orient='records')
+    return jsonify(countryBarChart)
 
-
-@app.route("/contact")
-def contact():
-    email = "peleke@example.com"
-
-    return f"Questions? Comments? Complaints? Shoot an email to {email}."
-
-
-# 4. Define main behavior
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=8000, debug=True)
